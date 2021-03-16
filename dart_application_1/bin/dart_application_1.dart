ffcce76b093a79d1176229097089f71c3bf4231e
niaardiati <ardiatinia01@gmail.com>
2021-03-16 09:00:31 +0700

    edit

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,7 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  String input = stdin.readLineSync();
+  int number = int.tryParse(input);
+  print(number + 10);
 }
